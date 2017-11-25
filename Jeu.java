f34368655f99ee3c2ee42936c099646b526ce288
Valentin Dacheux <valentin.dacheux@gmail.com>
2017-11-25 03:22:33 +0100

    La méthode 'genererNombreMystere' n'utilise maintenant que les chiffres choisis dans la variable 'nombresUtilises'

--- Jeu.java
+++ Jeu.java
@@ -2,8 +2,8 @@ import java.util.Random;
 
 public class Jeu {
 
-	int longueurNombreMystere = 4;
-	int nombreUtilises[] = {0,1,2,3,4};
+	int longueurNombreMystere = 6;
+	int nombreUtilises[] = {0,1,2,3,4,5,6};
 	long nombreMystere;
 	
 	public Jeu() {
@@ -30,20 +30,29 @@ public class Jeu {
 	//Génération du nombre mystère
 	public void genererNombreMystere() {
 		
-		String nombreMystereString;
-		long maximumPossible;
-		String maximumPossibleString;
+		int maximumPossible;
 		
 		maximumPossible = nombreUtilises.length-1;//On récupère le nombre maximum à être utilisé dans nombreUtilises
-		maximumPossibleString = ""+maximumPossible+maximumPossible+maximumPossible+maximumPossible+maximumPossible+maximumPossible+maximumPossible+maximumPossible+maximumPossible+maximumPossible;/*On l'ajoute
-		le plus de fois possible à un String
-		*/
-		maximumPossible = Long.parseLong(maximumPossibleString);//On remet les valeurs numériques du String dans le Long maximumPossible
 		
 		Random random = new Random();
-		nombreMystere = 0000000000 + (long)(random.nextDouble()*(maximumPossible - 0000000000L));//On génère un nombre aléatoire qui va jusqu'au maximum possible
-		nombreMystereString = String.valueOf(nombreMystere);//On le convertit en String
-		nombreMystere = Long.parseLong((nombreMystereString).substring(0,longueurNombreMystere));//On le reconvertit en Long en ne gardant que le nombre de caractères correspondant à longueurNombreMystere
+		
+		int chiffreNombreMystere[] = new int [longueurNombreMystere];
+		String sChiffre = "";
+		
+		for (int i = 0; i < longueurNombreMystere; i++) {//On génère un chiffre aléatoire jusqu'à atteindre la longueur définie dans longueurNombreMystere
+			chiffreNombreMystere[i] = random.nextInt(maximumPossible+1);
+			
+			sChiffre += chiffreNombreMystere[i];//On ajoute le chiffre généré au String sChiffre
+			System.out.println(sChiffre);
+		}
+		
+		//Si le premier chiffre aléatoire est un zéro, il est remplacé par un autre chiffre aléatoire entre 1 et 9 (sinon il ne s'affichera pas)
+		if (sChiffre.charAt(0) == '0') {
+			int r = random.nextInt(9-1)+1;
+			sChiffre = r+sChiffre.substring(1, sChiffre.length());
+		}
+		
+		nombreMystere = Long.parseLong((sChiffre));//On convertit sChiffre en Long dans nombreMystere
 
 	}
 	
