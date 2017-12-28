986f03a5d6acca10004218a88f44fd124fdb23ec
Valentin Dacheux <valentin.dacheux@gmail.com>
2017-12-28 12:50:59 +0100

    Corrigé une erreur concernant Erreur.java (le fichier n'était pas indexé correctement dans git)

---
+++ Erreur.java
@@ -0,0 +1,11 @@
+public abstract class Erreur {
+
+		//Message affiché si une entrée clavier ne correspond à aucun choix proposé 
+	public static void erreurChoix() {
+		System.out.println("\nVeuillez choisir parmi les propositions.");
+	}
+	
+	public static void erreurNombre() {
+		System.out.println("\nVeuillez entrer un nombre à "+Jeu.longueurNombreMystere+" chiffres.");
+	}
+}
